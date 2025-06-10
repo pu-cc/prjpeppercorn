58a098407b868664732dce19f7f60896c6bd6ae5
Miodrag Milanovic <mmicko@gmail.com>
2025-06-10 10:08:28 +0200

    Bump version to 1.2

--- gatemate/chip.py
+++ gatemate/chip.py
@@ -23,7 +23,7 @@ from dataclasses import dataclass
 from typing import List, Dict
 from timing import decompress_timing
 
-DATABASE_VERSION = 1.1
+DATABASE_VERSION = 1.2
 
 @dataclass(eq=True, order=True)
 class Pad:
