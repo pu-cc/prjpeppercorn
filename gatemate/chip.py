f6654f83a7bd9506cd12d9a004206744c0ef50f0
Miodrag Milanovic <mmicko@gmail.com>
2025-09-02 14:04:37 +0200

    bump chipdb

--- gatemate/chip.py
+++ gatemate/chip.py
@@ -23,7 +23,7 @@ from dataclasses import dataclass
 from typing import List, Dict
 from timing import decompress_timing
 
-DATABASE_VERSION = 1.5
+DATABASE_VERSION = 1.6
 
 @dataclass(eq=True, order=True)
 class Pad:
